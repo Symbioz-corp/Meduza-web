9c46756f34d8ccae02f1d51672c7d0767c68583e
andritianaa <andritianasteve@gmail.com>
2023-07-31 07:13:19 +0300

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import React from "react"
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import FAQ from "./components/home/FAQ"
-import Footer from "./components/home/footer"
-import Hero from "./components/home/hero"
-import ImageXL from "./components/home/ImageXL"
-import Pricing from "./components/home/prix"
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Suspense, lazy } from 'react'
+
+
+const Home = lazy(() => import('./pages/informative/Home'))
 
 export default function App() {
 
   return (
-    <>
-      <Hero />
-      <Pricing />
-      <ImageXL />
-      <FAQ />
-      <Footer />
-    </>
+    <BrowserRouter>
+      <AppContent />
+    </BrowserRouter>
   )
 }
 
+function AppContent() {
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Suspense> <Home /></Suspense>}></Route>
+      </Routes>
+    </>
+  )
+}
