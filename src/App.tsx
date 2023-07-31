83f07edb60a8f7e8d610c422231404aa0922b787
andritianaa <andritianasteve@gmail.com>
2023-07-31 07:04:44 +0300

    fix import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
 import FAQ from "./components/home/FAQ"
 import Footer from "./components/home/footer"
 import Hero from "./components/home/hero"
